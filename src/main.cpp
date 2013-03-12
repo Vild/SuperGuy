51b488ddea5f574cda65d53765da01c7097b04a8
Dan Printzell <xwildn00bx@gmail.com>
2013-03-12 20:04:05 +0100

    + Added base of project
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/* ============================================================================
+ * Name        : main.cpp
+ * Author(s)   : Dan "WildN00b" Printzell
+ * Copyright   : GPLv2, i think
+ * Description : 
+ * ============================================================================ */
+
+#include <iostream>
+#include <SDL/SDL.h>
+
+int main(int argc, char ** argv) {
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
