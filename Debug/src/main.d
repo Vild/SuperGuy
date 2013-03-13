66a4e074ab8a67d7af338bc7b59b95576ed1ecdc
ludkiller <ludkiller@dev.(none)>
2013-03-13 08:32:56 +0530

    Added log.cpp amd log.h

---
+++ Debug/src/main.d
@@ -0,0 +1,3 @@
+src/main.d: ../src/main.cpp ../src/log.h
+
+../src/log.h:
---
+++ src/log.h
@@ -0,0 +1,23 @@
+/*
+ * log.h
+ *
+ *  Created on: Mar 13, 2013
+ *      Author: ludkiller
+ */
+
+#ifndef LOG_H_
+#define LOG_H_
+
+#define DEBUG = 1;
+class log	{
+
+	public:
+		void loginfo(const char *);
+		void logerror(const char *);
+		log();
+		~log();
+
+};
+
+
+#endif /* LOG_H_ */
