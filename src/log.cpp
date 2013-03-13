33e251ef7a8e0f935640a1705c06e65f5dc14985
Dan Printzell <xwildn00bx@gmail.com>
2013-03-13 20:46:47 +0100

    * Fixed log Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/log.cpp
+++ src/log.cpp
@@ -3,26 +3,22 @@
  *
  *  Created on: Mar 13, 2013
  *      Author: ludkiller
+ *      Author: WildN00b
  */
-#include<stdio.h>
-#include"log.h""
-#include<iostream>
+#include <stdio.h>
+#include "log.h"
+#include <iostream>
 
 void log::loginfo(const char *msg) {
-
 	std::cout << "[*] " << msg << std::endl;
-
 }
 
 void log::logerror(const char*msg) {
-
 	std::cout << "[!] " << msg << std::endl;
 }
 
-log::log() {
-	return;
+log::log() {	
 }
 
 log::~log() {
-	return;
 }
