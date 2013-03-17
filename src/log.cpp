91b8c10473589b2bed656e587f86edff32d78d7e
Dan Printzell <xwildn00bx@gmail.com>
2013-03-17 14:16:40 +0100

    * Fixed log printing + Added time and date to the log printing Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/log.cpp
+++ src/log.cpp
@@ -9,31 +9,44 @@
 #include "log.h"
 #include <cstdio>
 #include <stdarg.h>
+#include <ctime>
+
+static char buf[80];
+
+static char * getTime() {
+	time_t rawtime;
+	struct tm * timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buf, 80, "%Ex - %EX", timeinfo);
+	return buf;
+}
 
 void log::info(const char * msg, ...) {
 	va_list va;
 	va_start(va, msg);
-	puts("[*] ");
+	printf("%-18s [*] ", getTime());
 	vprintf(msg, va);
-	puts("\n");
+	printf("\n");
 	va_end(va);
 }
 
 void log::error(const char * msg, ...) {
 	va_list va;
 	va_start(va, msg);
-	puts("[!] ");
+	printf("%-18s [!] ", getTime());
 	vprintf(msg, va);
-	puts("\n");
+	printf("\n");
 	va_end(va);
 }
 
 void log::warning(const char * msg, ...) {
 	va_list va;
 	va_start(va, msg);
-	puts("[#] ");
+	printf("%-18s [#] ", getTime());
 	vprintf(msg, va);
-	puts("\n");
+	printf("\n");
 	va_end(va);
 }
 
