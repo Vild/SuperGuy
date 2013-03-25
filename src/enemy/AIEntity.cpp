cecacc4eaab842899c402d37ece0b4ec053cb61e
ludkiller <ludkiller@dev.(none)>
2013-03-25 10:35:18 +0530

    Added AIEntity and LivingEntity

---
+++ src/enemy/AIEntity.cpp
@@ -0,0 +1,17 @@
+/*
+ * AIEntity.cpp
+ *
+ *  Created on: Mar 25, 2013
+ *      Author: ludkiller
+ */
+
+#include<SDL2/SDL.h>
+#include"AIEntity.h"
+
+aiEntity::aiEntity() {
+
+}
+
+aiEntity::~aiEntity() {
+
+}
---
+++ src/enemy/livingEntity.cpp
@@ -0,0 +1,10 @@
+/*
+ * livingEntity.cpp
+ *
+ *  Created on: Mar 25, 2013
+ *      Author: ludkiller
+ */
+
+
+
+
